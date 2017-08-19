953ca7663a70a18c08d961d25d467fcf8a89a81e
Pedro Flores <pdavidflores@gmail.com>
2017-08-18 22:21:22 -0400

    Added gkm module selection

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 'use strict';
-const ioHook = require('iohook');
+const config = require('./config');
 const robot = require("robotjs");
 
-let toggleClick = true;
- 
-ioHook.on("mousemove", event => {
-  console.log(event);
-});
+console.log(config.hook);
 
-ioHook.on("keydown", event => {
-  console.log(event);
-  if(event.keycode === 68) {
-    if(toggleClick) {
-     robot.mouseToggle('down'); 
-     toggleClick = !toggleClick
-    } else {
-     robot.mouseToggle('up'); 
-     toggleClick = !toggleClick
+if(config.hook === 'iohook') {
+  const ioHook = require('iohook');  
+  
+  ioHook.on("mousemove", event => {
+    console.log(event);
+  });
+
+  ioHook.on("keydown", event => {
+    console.log(event);
+    if(event.keycode === 68) {
+      autoClick();
     }
-  }
-});
+  });
 
-ioHook.start();
+  ioHook.start();
+}
+
+if(config.hook === 'gkm') {
+  const gkm = require("gkm");
+  // Listen to all key events (pressed, released, typed)
+  gkm.events.on('key.*', function(data) {
+      console.log(this.event + ' ' + data);
+      if(this.event + ' ' + data === 'key.pressed F10') {
+        autoClick();
+      }
+  });
+
+  // Listen to all mouse events (click, pressed, released, moved, dragged)
+  // gkm.events.on('mouse.*', function(data) {
+  //   console.log(this.event + ' ' + data);
+  // });
+}
+
+let toggleClick = true;
+
+function autoClick() {
+  if(toggleClick) {
+    robot.mouseToggle('down'); 
+    toggleClick = !toggleClick
+  } else {
+    robot.mouseToggle('up'); 
+    toggleClick = !toggleClick
+  }
+}
