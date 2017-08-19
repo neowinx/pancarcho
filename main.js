d2a52ebc56f190f92f681b1422f689e6ffb1d61f
Pedro Flores <pdavidflores@gmail.com>
2017-08-19 14:57:03 -0400

    base coords and mole mode added

--- main.js
+++ main.js
@@ -23,18 +23,25 @@ if(config.hook === 'iohook') {
 
 if(config.hook === 'gkm') {
   const gkm = require("gkm");
-  // Listen to all key events (pressed, released, typed)
-  gkm.events.on('key.*', function(data) {
-      console.log(this.event + ' ' + data);
-      if(this.event + ' ' + data === 'key.pressed F10') {
-        autoClick();
-      }
+  gkm.events.on('key.pressed', function(data) {
+    //console.log(this.event + ' ' + data);
+    if(this.event + ' ' + data === 'key.pressed F10') {
+      tpBase();
+    }
+    if(this.event + ' ' + data === 'key.pressed Numpad 0') {
+      moleMode();
+    }
   });
 
-  // Listen to all mouse events (click, pressed, released, moved, dragged)
-  // gkm.events.on('mouse.*', function(data) {
-  //   console.log(this.event + ' ' + data);
-  // });
+  gkm.events.on('mouse.pressed', function(data) {
+    //console.log(this.event + ' ' + data);
+    if(this.event + ' ' + data === 'mouse.pressed 4') {
+      autoClick();
+    }
+    if(this.event + ' ' + data === 'mouse.pressed 5') {
+      autoRun();
+    }
+  });
 }
 
 let toggleClick = true;
@@ -48,3 +55,43 @@ function autoClick() {
     toggleClick = !toggleClick
   }
 }
+
+let toggleRun = true;
+
+function autoRun() {
+  if(toggleRun) {
+    robot.keyToggle('w', 'down');
+    robot.keyToggle('shift', 'down'); 
+    toggleRun = !toggleRun
+  } else {
+    robot.keyToggle('w', 'up');
+    robot.keyToggle('shift', 'up'); 
+    toggleRun = !toggleRun
+  }
+}
+
+let moleMode = true;
+let advanceInterval;
+function moleMode() {
+  if(moleMode) {
+    robot.mouseToggle('down'); 
+    advanceInterval = setInterval(function(){
+      robot.setKeyboardDelay(300);
+      robot.keyToggle('w', 'down');
+      robot.keyToggle('w', 'up');
+    }, 1000);
+  } else {
+    clearInterval(advanceInterval);
+    robot.mouseToggle('up'); 
+  }
+}
+
+function tpBase() {
+  robot.setKeyboardDelay(800);
+  robot.keyTap('f1');
+  robot.typeString(`tp ${config.base.x} ${config.base.z}`);
+  robot.keyTap('enter');
+  robot.keyTap('f1');
+}
+
+
